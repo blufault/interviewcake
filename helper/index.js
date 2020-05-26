60fb217e0971c153ed67ebb0de49df7fabfaaaa7
Jeffrey Kim <heffy.etc@gmail.com>
2020-05-26 15:28:15 -0700

    Added couple of helper functions
    * assert
    * isMapEqual

--- helper/index.js
+++ helper/index.js
@@ -9,6 +9,32 @@ function AssertDeepEqual(a, b, desc) {
   }
 }
 
+const isMapEqual = (map1, map2) => {
+  if (map1.size !== map2.size) {
+    return false;
+  }
+
+  for (let [key, val] of map1) {
+    const testVal = map2.get(key);
+
+    if (testVal !== val || (testVal === undefined && !map2.has(key))) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const assert = (condition, desc) => {
+  if (condition) {
+    console.log(`${desc} ... PASS`);
+  } else {
+    console.log(`${desc} ... FAIL`);
+  }
+};
+
 module.exports = {
-  AssertDeepEqual
+  AssertDeepEqual,
+  isMapEqual,
+  assert
 };
