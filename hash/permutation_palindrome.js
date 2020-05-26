70dd7e3bf3345b0d74e410140dd633be21ae648f
Jeffrey Kim <heffy.etc@gmail.com>
2020-05-26 12:44:26 -0700

    Completed palidrome permutation
    Took awhile to get the permutation for strings longer than 3 characters.
    There might be a better way of solving this problem. But did what came
    to mind first.

--- hash/permutation_palindrome.js
+++ hash/permutation_palindrome.js
@@ -1,49 +1,77 @@
 // Write an efficient function that checks whether any permutation of an input string is a palindrome.
 
+const { AssertDeepEqual } = require('../helper');
+
+// input: index and input
+// output: return an array
 const exclusive_get = (index, input) => {
-  return input.split('').reduce((accum, value, r_index) => {
+  return input.split('').reduce((accum, curr, r_index) => {
     if (r_index !== index) {
-      accum.push(value);
+      accum.push(curr);
     }
+
     return accum;
-  }, []);
+  }, []).join('');
 };
 
-const permutate = (index, mod_input, original_input) => {
-  // input[index] is what we want to keep
-  if (original_input.length === 1) {
-    return [original_input];
+// output: return an array
+const swap = (root, str) => {
+  return [root + str[0] + str[1], root + str[1] + str[0]];
+};
+
+const permutate = (root, rest) => {
+  if (rest.length > 2) {
+    let new_root = root;
+    return permutation(rest).reduce((accum, sub) => {
+      new_root += sub;
+      accum.push(new_root);
+      new_root = root;
+      return accum;
+    }, []);
   }
-  console.log('permuatate: ', { index: original_input[index], mod_input, original_input });
-  let string = '';
-  const result = [];
-
-  for (let i = 0; i< mod_input.length; i++) {
-    string = original_input[index];
-    if (i === 0) {
-      string += mod_input.join('');
-    } else {
-      string += mod_input[i] + mod_input[0]
-    }
-
-    result.push(string);
-  }
-
-  return result;
+  return swap(root, rest);
 };
 
 const permutation = (input) => {
   const set = [];
   for (let i = 0; i < input.length; i++) {
-    set.push(...permutate(i, exclusive_get(i, input), input));
+    set.push(...permutate(input[i], exclusive_get(i, input)));
   }
-
-  console.log({ set });
+  return set;
 };
 
-permutation('a');
-permutation('ab');
-permutation('abc');
-permutation('abcd');
-//permutation('tom');
+// input: string
+// output: return true or false if the string is a palidrome
+const isPalidrome = (input) => {
+  for (let i = 0; i < Math.ceil(input.length / 2); i++) {
+    const complement = (input.length - 1) - i;
+    if (input[i] !== input[complement]) {
+      return false;
+    }
+  }
 
+  return true;
+};
+
+const isPalidromeFromPermutation = (input) => {
+  const permutations = permutation(input);
+  let result = false;
+
+  permutations.forEach((permutation) => {
+    if (isPalidrome(permutation)) {
+      result = true;
+    }
+  });
+
+  return result;
+};
+
+let desc = 'civic';
+let actual = isPalidromeFromPermutation('civic');
+let expected = true;
+AssertDeepEqual(actual, expected, desc);
+
+desc = 'civil';
+actual = isPalidromeFromPermutation('civil');
+expected = false;
+AssertDeepEqual(actual, expected, desc);
