afab7f64d44c74036733b90b73d268f1516cedf5
Jeffrey Kim <heffy.etc@gmail.com>
2020-05-27 10:37:18 -0700

    Top score is working but...
    The time complexity is O(n^2).
    Might have an idea to make it O(n), use an array that has the size of n.

---
+++ hash/top_scores.js
@@ -0,0 +1,54 @@
+const { AssertDeepEqual } = require('../helper');
+
+// Time Complexity: O(n^2) because of the reduceRight and nested for-loop
+// Space Complexity: O(n)
+const sortScores = (unsortedScores, highestPossibleScore) => {
+  if (!unsortedScores.length) {
+    return [];
+  }
+
+  const cache = {};
+
+  unsortedScores.forEach((score) => {
+    if (cache[score]) {
+      cache[score]++;
+    } else {
+      cache[score] = 1;
+    }
+  });
+
+  console.log({ cache });
+  return Object.keys(cache).reduceRight((accum, key) => {
+    for (let i = 0; i < cache[key]; i++) {
+      accum.push(Number(key));
+    }
+    return accum;
+  }, []);
+};
+
+// Test
+let desc = 'empty scores';
+let actual = sortScores([], 55);
+let expected = [];
+AssertDeepEqual(actual, expected, desc);
+
+desc = 'one scores';
+actual = sortScores([55], 100);
+expected = [55];
+AssertDeepEqual(actual, expected, desc);
+
+desc = 'two scores';
+actual = sortScores([55, 90], 100);
+expected = [90, 55];
+AssertDeepEqual(actual, expected, desc);
+
+desc = 'many scores';
+actual = sortScores([37, 89, 41, 65, 91, 53], 100);
+expected = [91, 89, 65, 53, 41, 37];
+AssertDeepEqual(actual, expected, desc);
+
+desc = 'repeated scores';
+actual = sortScores([20, 10, 30, 30, 10, 20], 100);
+expected = [30, 30, 20, 20, 10, 10];
+AssertDeepEqual(actual, expected, desc);
+
